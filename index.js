dfb4ae97eee44bbfee7eb525ff87a7d6e878fa94
ahmad <ahmadmuradmalik@gmail.com>
2024-05-14 17:09:47 -0700

    changed default route and added flip coins and speak to ibrahim routes

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello from ibrahim ihsan!');
 });
 
+app.get('/FlipCoin', (req, res) => {
+    return res.json({result: 1});
+});
+
+app.post('/SpeakToIbrahim', (req, res) => {
+    const {userInput} = req.body;
+    return res.json({response: userInput})
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
